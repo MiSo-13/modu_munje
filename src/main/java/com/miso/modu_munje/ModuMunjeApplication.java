7b5ac7b96414f5fc0835656283fae81d1a576cf4
MiSo <jimrayder3701@gmail.com>
2023-02-09 14:44:37 +0900

    Initialize java + gradle
    java + gradle 프로젝트 셋팅

---
+++ src/main/java/com/miso/modu_munje/ModuMunjeApplication.java
@@ -0,0 +1,13 @@
+package com.miso.modu_munje;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuMunjeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuMunjeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miso/modu_munje/ModuMunjeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miso.modu_munje;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuMunjeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
